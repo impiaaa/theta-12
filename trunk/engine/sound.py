9aa52b51a1453d8675c91c15666d45a879c2490c
Gabsr505 <Gabsr505@9bc04876-1b4d-11df-8bc4-a52d3ea79963>
2010-03-09 01:17:43 +0000

    I changed the sound.py file to make it better

--- trunk/engine/sound.py
+++ trunk/engine/sound.py
@@ -1,45 +1,47 @@
 # ... means you need to code something
 
 
-def playSound(sound_id):
+def playSound(sound_id,loop,maxtim):
 	""" Plays the sound with the given string id """
-	# ...
-
-def isSoundPlaying(sound_id):
+    sound_id.play(loops=loop,maxtime=maxtim)
+	
+def isSoundPlaying():
 	""" Returns true if the sound with the id is playing """
-	# ...
+    pygame.mixer.get_busy()
 
 def stopSound(sound_id):
 	""" Stops the sound if it is playing, or does nothing if it is not """
-	# ...
+    sound_id.stop()
 
-def playTrack(track_id):
+def playTrack(track_id,loop):
 	""" Plays the music track with the given string id """
 	# ... The implementation of this should be
 	#     different than playSound() because it is bad to load
 	#	  a whole track into memory, so it must be streamed.
+    pygame.mixer.music.load(track_id)
+    pygame.mixer.music.play(loops=loop)
 
-def isTrackPlaying(track_id):
+def isTrackPlaying():
 	""" Returns true if the track id is playing """
-	# ...
+    pygame.mixer.music.get_busy()
 
 def stopTrack(track_id):
 	""" Stops the given track """
-	# ...
+    pygame.mixer.music.stop()
 
 def fadeOutTrack(track_id, time):
 	""" If the given id is playing, it fades out the track
 		over the given time interval (in ms) """
-	# ...
+    track_id.fadeout(time)
 
 def stopAllTracks():
 	""" Stops all the playing tracks """
-	# ...
+    pygame.mixer.music.stop()
 
 def fadeOutAllTracks(time):
 	""" Fades out all playing tracks over the given time interval (ms) """
-	# ...
+    pygame.mixer.fadeout(time)
 
 def stopAllSounds():
 	""" Stops all playing sounds """
-	# ...
+    pygame.mixer.stop()
