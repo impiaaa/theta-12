c7cda4b7e6b809142f11077e7718cae51e29d681
garrett.malmquist <garrett.malmquist@9bc04876-1b4d-11df-8bc4-a52d3ea79963>
2010-03-22 04:31:29 +0000

    Tuned up collision algorithms for entities.Block, improved scrolling, added elevator

--- trunk/engine/entities.py
+++ trunk/engine/entities.py
@@ -1,11 +1,21 @@
 import pygame
 import graphwrap, math
 
+currentid = 0
+game_entities = {}
+
+def entity_named(name):
+	""" Returns the game entity with the specified name. """
+	for i in game_entities:
+		if game_entities[i] == name:
+			return game_entities[i]
+
 class Entity:
 	def __init__(self, geom, anim):
 		""" geom should be a rectangle or a tuple in rectangle format: (x, y, w, h) or pygame.rect.Rect
 			anim should be an AnimSprite (see graphwrap.py) or None or -1. If it is None, a simple
-			bounding rectangle will be shown. If it is -1, it will be entirely invisible. """
+			bounding rectangle will be shown. If it is -1, it will be entirely invisible. 
+			All subclasses MUST call this constructer or things will BREAK. """
 		self.geom = geom # pygame.rect.Rect or tuple
 		if not isinstance(geom, pygame.rect.Rect):
 			self.geom = pygame.rect.Rect(geom) # convert to pygame.rect.Rect
@@ -18,6 +28,13 @@ class Entity:
 		self.sticking = False # true if it is sitting on a "sticky" surface
 		self._incx, self._incy = 0, 0 # necessary because pygame.rect.Rect uses integers =/
 		self.name = None # may be used for identification. Only used for "player", for now.
+		self.spawn = [] # this is a list of entities that should be spawned by the main loop.
+						  # this allows entities to create other entities and be multi-part.
+		self.attributes = [] # this contains a list of the names of what lists this should go in (geometry, activators, etc)
+							 # see level.Room.add()
+		#self.id = currentid
+		#game_entites[self.id] = self
+		#currentid += 1
 		
 	def draw(self, artist):
 		if self.anim is None:
@@ -32,9 +49,12 @@ class Entity:
 		if self.anim is not None and self.anim != -1:
 			self.anim.update(time)
 
-	def update(self, time):
+	def updatelast(self):
 		self.last.left = self.geom.left
 		self.last.top = self.geom.top
+
+	def update(self, time):
+		self.updatelast()
 
 		self.velx += self.acx * time
 		self.vely += self.acy * time
@@ -64,6 +84,7 @@ class Entity:
 class FloorBlock(Entity):
 	def __init__(self, geom, anim):
 		Entity.__init__(self, geom, anim)
+		self.attributes.append("geometry")
 
 	def intersects(self, other_entity):
 		if Entity.intersects(self, other_entity):
@@ -126,9 +147,10 @@ class Block(FloorBlock):
 		elif wright and not right:
 			other.geom.left = self.geom.right
 			other.velx = self.velx
-		elif wbelow and not below:
+		elif wbelow and not below or not (abs(other.geom.bottom - self.geom.top) < abs(self.geom.bottom - other.geom.top)):
 			other.geom.top = self.geom.bottom
 			other.vely = self.vely
+			other.updatelast()
 		else:
 			if not above:
 				other.geom.bottom = self.geom.top
@@ -157,3 +179,78 @@ class MotionTrigger(TriggerEntity):
 	def collision(self, other):
 		if other.name == "player":
 			TriggerEntity.trigger(self)
+
+class Elevator(Entity):
+	def __init__(self, geom, targety, duration):
+		""" Paramaters: geometry (rectangle),  targety (where the elevator will go), and duration (the 
+			time in seconds it will take to get there) """
+		Entity.__init__(self, geom, -1)
+		self.floor = Block((self.geom.left, self.geom.bottom-20, self.geom.width, 20), None)
+		self.roof = Block((self.geom.left, self.geom.top, self.geom.width, 20), None)
+		self.floor.sticky = True
+		self.roof.sticky = True
+		self.spawn.append(self.floor)
+		self.spawn.append(self.roof)
+		self.y1 = self.geom.top
+		self.y2 = targety
+		self.state = 0 # 0 = stopped, 1 = go towards position 1, -1 = go towards position 0
+		self.duration = duration
+		if self.y2 - self.y1 > 0:
+			self.dir = 1
+		else:
+			self.dir = -1
+		self.speed = abs( (self.y2 - self.y1) / duration)
+		self.attributes.append("geometry") # this is in so it can move automatically when the player steps on it
+		self.playerref = None			   # this will be taken out because we'll probably want the elevator to be
+										   # activated by external means a lot of the time.
+
+
+	def intersects(self, other_entity):
+		if Entity.intersects(self, other_entity):
+			return True
+		if self.geom.top - other_entity.geom.bottom <= 3 and (self.geom.right > other_entity.geom.right > self.geom.left
+				or self.geom.right > other_entity.geom.left > self.geom.left):
+			return True
+		return False
+
+	def collision(self, other):
+		if other.name == "player" and self.state == 0 and self.playerref == None:
+			if self.geom.y == self.y1:
+				self.state = 1
+			else:
+				self.state = -1
+			
+			self.playerref = other
+
+	def update(self, time):
+		Entity.update(self, time)
+		if self.state == 0:
+			self.vely = 0
+		elif self.state == 1:
+			if self.dir == 1 and self.geom.y < self.y2:
+				self.vely = self.speed
+			elif self.dir == -1 and self.geom.y > self.y2:
+				self.vely = -self.speed
+			else:
+				self.vely = 0
+				self.geom.y = self.y2
+				self.state = 0
+		elif self.state == -1:
+			if self.dir == 1 and self.geom.y > self.y1:
+				self.vely = -self.speed
+			elif self.dir == -1 and self.geom.y < self.y1:
+				self.vely = self.speed
+			else:
+				self.vely = 0
+				self.geom.y = self.y1
+				self.state = 0
+
+		self.floor.updatelast()
+		self.roof.updatelast()
+		self.floor.geom.top = self.geom.bottom - 20
+		self.roof.geom.top = self.geom.top
+
+		if self.playerref != None:
+			if not self.intersects(self.playerref):
+				self.playerref = None
+	
