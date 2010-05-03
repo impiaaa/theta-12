d3d3f7d748b2868eb243463dd79a9a9796187b50
garrett.malmquist@gmail.com <garrett.malmquist@gmail.com@9bc04876-1b4d-11df-8bc4-a52d3ea79963>
2010-05-03 22:39:13 +0000

    Added a sprites dict to t12

--- engine/t12.py
+++ engine/t12.py
@@ -5,6 +5,7 @@ ent_currentid = 0
 game_entities = {}
 seq_currentid = 0
 anim_sequences = {}
+sprites = {}
 gravity = 386 # 9.8 m/s, according to the art team's scale
 camy = 0 # vertical camera offset to be reset each time a room is loaded.
 seconds_passed = 0 # time passed in the last frame
